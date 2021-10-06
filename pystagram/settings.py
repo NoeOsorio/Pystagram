01d8b5253b81cce2f6840926f056116e1aacff46
Noe Osorio <osorionoe@gmail.com>
2021-10-05 23:39:54 -0500

    Connection with postgresql

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'pystagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pystagram',
+        'USER': 'noe',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
